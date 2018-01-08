6ab6b8693a6ab3cd4ef4d71fed6f50e73889fdc4
danielohayon <danielohayon444@gmail.com>
2018-01-08 17:50:24 +0200

    basic scelaton

--- Decryptor/Decryptor.cs
+++ Decryptor/Decryptor.cs
@@ -4,6 +4,7 @@
     {
         public static void Main(string[] args)
         {
+            System.Console.WriteLine("Your feils have been encryptrd!");
         }
     }
 }
--- Encryptor/Encryptor.cs
+++ Encryptor/Encryptor.cs
@@ -4,6 +4,8 @@
     {
         public static void Main(string[] args)
         {
+            System.Console.WriteLine("Your feils have been encryptrd!");
+
         }
     }
 }
